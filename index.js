81ec57400d1acaaee3afd27e6e2ba05c0349be1d
H0v <tonoyan6666@gmail.com>
2020-10-04 19:33:36 +0400

    commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const moment = require("moment");
+const stream = require("stream");
+// moment.locale("ru");
+// console.log(moment().format("dddd, MMMM DD YYYY, h:mm:ss"));
+
+class ReadableStream extends stream.Readable {
+  _read() {
+    console.log("reading");
+  }
+}
+
+class TransformStream extends stream.Transform {
+  _transform(chunk, enc, callback) {
+    const data = chunk.toString();
+    callback(null, `[Date : ${data}]`);
+  }
+}
+
+class WritableStream extends stream.Writable {
+  _write(chunk, enc, callback) {
+    console.log("chunk:", chunk.toString());
+    callback(null);
+  }
+}
+
+const readableStream = new ReadableStream();
+const transformStream = new TransformStream();
+const writableStream = new WritableStream();
+
+// setInterval(() => {
+//   readableStream.push("Hello World");
+// }, 2000);
+// readableStream.pipe(writableStream);
+
+setInterval(() => {
+  readableStream.push(moment().format("dddd, MMMM DD YYYY, h:mm:ss"));
+}, 2000);
+readableStream.pipe(transformStream).pipe(writableStream);
